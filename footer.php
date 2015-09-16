46d795550fb55afbc0d2c80673cb8dceaa79b0bd
Rovniy <smart.fz@mail.ru>
2015-09-16 23:08:44 +0300

    footer

--- footer.php
+++ footer.php
@@ -1,18 +1,16 @@
 <!--  Нижний колонтитул  -->
 <div class="container-fluid footer_bar">
-	<div class="container footer">
-		<div class="row">
-			<div class="col-lg-3 col-sm-12">
+	<div class="row footer">
+			<div class="col-lg-4 col-sm-12">
 				<p>Dota777.com</p>
 			</div>
-			<div class="col-lg-6 col-sm-12">
+			<div class="col-lg-4 col-sm-12 center">
 				<p>Все права защищены. Любое копирование материала возможно только с письменного согласия владельцев сайта</p>
 			</div>
-			<div class="col-lg-3 col-sm-12">
-				<p>xPloit development</p>
+			<div class="col-lg-4 col-sm-12 pull-right">
+				<p class="pull-right">xPloit development</p>
 			</div>
 		</div>
-	</div>
 </div>
 <!--  Конец body  -->
 </body>
