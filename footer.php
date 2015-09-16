92b9ae20b37e6ffc4e2e8af12e5d1e843c6539d4
Rovniy <smart.fz@mail.ru>
2015-09-16 20:09:06 +0300

    Стилизация меню в виде Dota2 Reborn

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 <!--  Нижний колонтитул  -->
-<div class="container-fluid footer">
-<div class="container">
-	<div class="row">
-		<div class="col-lg-3">
-			<p>Dota777.com</p>
+<div class="container-fluid footer_bar">
+	<div class="container footer">
+		<div class="row">
+			<div class="col-lg-3 col-sm-12">
+				<p>Dota777.com</p>
+			</div>
+			<div class="col-lg-6 col-sm-12">
+				<p>Все права защищены. Любое копирование материала возможно только с письменного согласия владельцев сайта</p>
+			</div>
+			<div class="col-lg-3 col-sm-12">
+				<p>xPloit development</p>
+			</div>
 		</div>
-		<div class="col-lg-6">
-			<p>Все права защищены. Любое копирование материала возможно только с письменного согласия владельцев сайта</p>
-		</div>
-		<div class="col-lg-3">
-			<p>xPloit development</p>
-		</div>
-	</div>
 	</div>
 </div>
 <!--  Конец body  -->
--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Dota2.gg</title>
+<link type="image/x-icon" rel="shortcut icon" href="favicon.png">
 <link href="css/bootstrap.css" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet">
 <script src="js/jquery-1.11.0.min.js"></script>
@@ -18,25 +19,28 @@ include '/sys/api.php';
 ?>
 <!-- Шапка сайта, главное меню и логотип -->
 <div class="container-fluid header_bar">
-	<a href="#" class="pull-right steam_enter">Steam</a>
-	<div class="container header">
-		<div class="row">
-			<div class="col-lg-2 logo">
+	
+		<div class="row header">
+			<div class="col-lg-1 logo center">
+				<i class="fa fa-user"></i>
+			</div>
+			<div class="col-lg-1 logo center">
 				<img src="img/logo.png" alt=""/>
 			</div>
-			<div class="col-lg-10 menunav">
+			<div class="col-lg-8 menunav col-sm-12">
 				<ul>
-					<li><a href="#" class="active">Главная</a></li>
-					<li><a href="#">Помощь</a></li>
-					<li><a href="#">Инструкция</a></li>
-					<li><a href="#">Гарантии</a></li>
-					<li><a href="#">ТОП игроков</a></li>
-					<li><a href="#">Личный кабинет</a></li>
-					<li><a href="#">Мы Вконтакте</a></li>
+					<li class="active"><a href="#" >ГЛАВНАЯ</a></li>
+					<li><a href="#">ПОМОЩЬ</a></li>
+					<li><a href="#">ИНСТРУКЦИЯ</a></li>
+					<li><a href="#">ГАРАНТИИ</a></li>
+					<li><a href="#">ТОП</a></li>
+					<li><a href="#">ЛИЧНЫЙ КАБИНЕТ</a></li>
 				</ul>
 			</div>
+			<div class="col-lg-2 logo center">
+				<a href="#" class="pull-right steam_enter"><img src="img/steam_login.png"/></a>
+			</div>
 		</div>
-	</div>
 	
 	
 </div>
