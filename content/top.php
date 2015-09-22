a4896873f0ca15ee703f6a0c02a43c3866dd58cd
Rovniy <smart.fz@mail.ru>
2015-09-22 22:48:00 +0300

    Начал делать ТОП

--- content/top.php
+++ content/top.php
@@ -1,7 +1,49 @@
 <div class="row GarantTitle">
-	<h3>ТОП УЧАСТНИКОВ | КТО И ЧТО ВЫИГРЫВАЕТ В НАШЕЙ ЛОТЕРЕЕ</h3>
+	<table>
+		<tr class="TopHeader">
+			<td>
+				<span class="ColPlayers"># ИГРОКОВ В РЕЙТИНГЕ</span>
+			</td>
+			<td>
+				<span class="ColGames"># ИГР СЕГОДНЯ</span>
+			</td>
+			<td>
+				<span class="ColUnicums"># УНИКАЛЬНЫХ ИГРОКОВ СЕГОДНЯ</span>
+			</td>
+		</tr>
+		</table>
 </div>
-<div class="row GarantBody">
-	<p>ТОП ИГРОКОВ</p>
+<div class="row TopBody">
+	<table>
+		<tr class="ToptableTitle">
+			<td>
+				<span class="TopID">ID#</span>
+			</td>
+			<td>
+				<span class="TopNameSteam">ИМЯ В STEAM</span>
+			</td>
+			<td>
+				<span class="TopColItems">КОЛИЧЕСТВО ПРЕДМЕТОВ</span>
+			</td>
+			<td>
+				<span class="TopSummWin">СУММА ВЫИГРЫША</span>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<span class="TopID">1</span>
+			</td>
+			<td>
+				<span class="TopNameSteam">Ravy</span>
+			</td>
+			<td>
+				<span class="TopColItems">12</span>
+			</td>
+			<td>
+				<span class="TopSummWin">98414 руб.</span>
+			</td>
+		</tr>
+		
+	</table>
 </div>
 			
